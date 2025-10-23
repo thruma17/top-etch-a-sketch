4da298c1de2a2111a1376f71789de2c09b1de4ba
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-23 19:39:57 +0200

    Start creating divs

---
+++ script.js
@@ -0,0 +1,13 @@
+console.clear;
+
+const grid = document.getElementById(`container`);
+
+function createDivs(numOfDivs) {
+  for (let i = 0; i < numOfDivs; i++) {
+    const newDiv = document.createElement(`div`);
+    newDiv.className = `grid-pixel`;
+    grid.appendChild(newDiv);
+  }
+}
+
+createDivs(16);
