1ee73871e1eefeedcacecf7b6979b5f2b625cfac
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-24 10:45:19 +0200

    Create base css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.clear;
 const grid = document.getElementById(`container`);
 
 function createDivs(numOfDivs) {
-  for (let i = 0; i < numOfDivs; i++) {
+  for (let i = 0; i < numOfDivs ** 2; i++) {
     const newDiv = document.createElement(`div`);
     newDiv.className = `grid-pixel`;
     grid.appendChild(newDiv);
