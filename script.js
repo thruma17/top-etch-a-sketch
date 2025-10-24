14e18720f9d55d4e5d310755c07818e3fe5e231b
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-24 12:27:55 +0200

    Create function to ask user prompt for grid size

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function createGrid(size) {
   for (let i = 0; i < size ** 2; i++) {
     const pixel = document.createElement(`div`);
     pixel.className = `grid-pixel`;
-    pixel.style.flex = `0 0 ${(100 / size).toFixed(2)}%`;
+    pixel.style.flex = `0 0 ${(100 / size).toFixed(3)}%`;
     grid.appendChild(pixel);
     grid.addEventListener("click", () => {
       pixel.onmouseleave = function () {
@@ -20,6 +20,12 @@ function createGrid(size) {
 }
 
 createGrid(size);
+
+const densityButton = document.getElementById("btn-squares");
+densityButton.addEventListener("click", function () {
+  customSize = prompt(`Choose a number between 2 and 64`, `16`);
+  createGrid(customSize);
+});
 
 const cleanButton = document.getElementById("btn-reset");
 cleanButton.addEventListener("click", function () {
