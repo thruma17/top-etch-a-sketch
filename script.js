f3316ebc6f2524be57d2383f8907ffaa80c85b0d
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-24 18:29:35 +0200

    Refactor js

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 console.clear();
 
-let size = 16;
+// defaults
 
+const DEFAULT_SIZE = 16;
+
+let currentSize = DEFAULT_SIZE;
+
+// variables
+
+const toolButtons = document.querySelectorAll(".toolButtons");
+const penBtn = document.getElementById(`penBtn`);
+const pencilBtn = document.getElementById(`pencilBtn`);
+const rainbowBtn = document.getElementById(`rainbowBtn`);
+const eraserBtn = document.getElementById(`eraserBtn`);
+const clearBtn = document.getElementById(`clearBtn`);
+const densityBtn = document.getElementById(`densityBtn`);
 const grid = document.getElementById(`container`);
 
-function createGrid(size) {
-  grid.innerHTML = "";
-  for (let i = 0; i < size ** 2; i++) {
+// settings functions
+
+function setCurrentSize(newSize) {
+  currentSize = newSize;
+}
+
+//
+
+clearBtn.onclick = () => setupGrid(currentSize);
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+//
+
+function clearGrid() {
+  grid.innerHTML = ``;
+}
+
+function setupGrid(currentSize) {
+  clearGrid();
+  for (let i = 0; i < currentSize ** 2; i++) {
     const pixel = document.createElement(`div`);
     pixel.className = `grid-pixel`;
-    pixel.style.flex = `0 0 ${(100 / size).toFixed(3)}%`;
+    pixel.style.flex = `0 0 ${(100 / currentSize).toFixed(3)}%`;
     grid.appendChild(pixel);
-    eraserButton = document.getElementById("btn-eraser");
-    eraserButton.addEventListener("click", () => {
+    eraserBtn.addEventListener(`click`, () => {
       pixel.onmouseleave = function () {
         this.style.backgroundColor = `white`;
       };
     });
-    grid.addEventListener("click", () => {
+    grid.addEventListener(`click`, () => {
       pixel.onmouseleave = function () {
         this.style.backgroundColor = `black`;
       };
@@ -25,20 +57,21 @@ function createGrid(size) {
   }
 }
 
-createGrid(size);
-
-const densityButton = document.getElementById("btn-squares");
-densityButton.addEventListener("click", function () {
+function changeGrid() {
   customSize = prompt(`Choose a number between 2 and 64`, `16`);
   if (customSize > 1 && customSize < 65) {
-    createGrid(customSize);
+    setupGrid(customSize);
+    currentSize = customSize;
   } else {
     alert(`Invalid number, creating default pixel density`);
-    createGrid(size);
+    setupGrid(currentSize);
   }
-});
+}
 
-const cleanButton = document.getElementById("btn-reset");
-cleanButton.addEventListener("click", function () {
-  createGrid(size);
-});
+// start
+
+setupGrid(DEFAULT_SIZE);
+
+// event listeners
+
+densityBtn.addEventListener(`click`, changeGrid);
