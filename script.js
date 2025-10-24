5896d1a219da649bea9e813d3cf9d8d6c94373d4
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-24 11:17:34 +0200

    Clean function to crate drawing grid

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 console.clear;
 
+let size = 16;
+
 const grid = document.getElementById(`container`);
 
-function createDivs(numOfDivs) {
-  for (let i = 0; i < numOfDivs ** 2; i++) {
-    const newDiv = document.createElement(`div`);
-    newDiv.className = `grid-pixel`;
-    grid.appendChild(newDiv);
+function createGrid(size) {
+  for (let i = 0; i < size ** 2; i++) {
+    const pixel = document.createElement(`div`);
+    pixel.className = `grid-pixel`;
+    pixel.style.flex = `0 0 ${(100 / size).toFixed(2)}%`;
+    grid.appendChild(pixel);
   }
 }
 
-createDivs(16);
+createGrid(size);
