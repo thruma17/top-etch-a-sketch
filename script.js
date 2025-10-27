a8dff9f8205de8bd1ad5b3c6626ed111d86a7d3b
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-27 17:03:19 +0100

    Refactor code, add play and pause

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ console.clear();
 
 // defaults
 
+const DEFAULT_TOOL = `pen`;
 const DEFAULT_SIZE = 16;
 
+let currentTool = DEFAULT_TOOL;
 let currentSize = DEFAULT_SIZE;
+
+function setCurrentTool(newTool) {
+  chooseTool(newTool);
+  currentTool = newTool;
+}
 
 // variables
 
@@ -17,54 +24,28 @@ const clearBtn = document.getElementById(`clearBtn`);
 const densityBtn = document.getElementById(`densityBtn`);
 const grid = document.getElementById(`container`);
 
+let drawing = false;
+grid.onclick = () => (drawing = true);
+grid.ondblclick = () => (drawing = false);
+grid.onmouseleave = () => (drawing = false);
+
 //
 
-function clearGrid() {
-  grid.innerHTML = ``;
-}
-
 function setupGrid(currentSize) {
-  clearGrid();
+  grid.innerHTML = ``;
   for (let i = 0; i < currentSize ** 2; i++) {
     const newPixel = document.createElement(`div`);
     newPixel.id = `grid-pixel`;
     newPixel.className = `grid-pixel`;
-    const pixel = newPixel;
-    pixel.style.flex = `0 0 ${(100 / currentSize).toFixed(3)}%`;
-    grid.appendChild(pixel);
-    penBtn.addEventListener(`click`, () => {
-      pixel.onmouseenter = function () {
-        pixel.style.backgroundColor = `black`;
-        pixel.style.opacity = 1;
-      };
-    });
-    pencilBtn.addEventListener(`click`, () => {
-      pixel.onmouseenter = function () {
-        pixel.style.backgroundColor = `black`;
-        if (pixel.style.opacity <= 0.9) {
-          pixel.style.opacity = +pixel.style.opacity + 0.1;
-        } else if (
-          (pixel.style.opacity = 1 && pixel.style.backgroundColor === `black`)
-        ) {
-          pixel.style.opacity = 1;
-        }
-      };
-    });
-    rainbowBtn.addEventListener(`click`, () => {
-      pixel.onmouseenter = function () {
-        const randomR = Math.floor(Math.random() * 256);
-        const randomG = Math.floor(Math.random() * 256);
-        const randomB = Math.floor(Math.random() * 256);
-        pixel.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-        pixel.style.opacity = 1;
-      };
-    });
-    eraserBtn.addEventListener(`click`, () => {
-      pixel.onmouseenter = function () {
-        pixel.style.opacity = 0;
-      };
-    });
+    newPixel.style.flex = `0 0 ${(100 / currentSize).toFixed(3)}%`;
+    grid.appendChild(newPixel);
+    newPixel.addEventListener("mouseleave", draw);
   }
+}
+
+function clearGrid() {
+  grid.innerHTML = ``;
+  setupGrid(currentSize);
 }
 
 function changeGrid() {
@@ -78,15 +59,82 @@ function changeGrid() {
   }
 }
 
+function chooseTool(newTool) {
+  switch (currentTool) {
+    case `pen`:
+      penBtn.classList.remove(`active`);
+      break;
+    case `pencil`:
+      pencilBtn.classList.remove(`active`);
+      break;
+    case `rainbow`:
+      rainbowBtn.classList.remove(`active`);
+      break;
+    case `eraser`:
+      eraserBtn.classList.remove(`active`);
+      break;
+  }
+
+  switch (newTool) {
+    case `pen`:
+      penBtn.classList.add(`active`);
+      break;
+    case `pencil`:
+      pencilBtn.classList.add(`active`);
+      break;
+    case `rainbow`:
+      rainbowBtn.classList.add(`active`);
+      break;
+    case `eraser`:
+      eraserBtn.classList.add(`active`);
+      break;
+  }
+}
+
+function draw(e) {
+  if (e.type === "mouseleave" && !drawing) return;
+  switch (currentTool) {
+    case `pen`:
+      e.target.style.backgroundColor = `black`;
+      e.target.style.opacity = 1;
+      break;
+    case `pencil`:
+      e.target.style.backgroundColor = `black`;
+      if (e.target.style.opacity <= 0.9) {
+        e.target.style.opacity = +e.target.style.opacity + 0.1;
+      } else if (
+        (e.target.style.opacity =
+          1 && e.target.style.backgroundColor === `black`)
+      ) {
+        e.target.style.opacity = 1;
+      }
+      break;
+    case `rainbow`:
+      const randomR = Math.floor(Math.random() * 256);
+      const randomG = Math.floor(Math.random() * 256);
+      const randomB = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+      e.target.style.opacity = 1;
+      break;
+    case `eraser`:
+      e.target.style.backgroundColor = `white`;
+      e.target.style.opacity = 0;
+      break;
+  }
+}
+
 // start
 
-setupGrid(DEFAULT_SIZE);
+window.onload = () => {
+  setupGrid(DEFAULT_SIZE);
+  chooseTool(DEFAULT_TOOL);
+};
 
 // event listeners
 
-clearBtn.onclick = () => setupGrid(currentSize);
+penBtn.onclick = () => setCurrentTool("pen");
+pencilBtn.onclick = () => setCurrentTool("pencil");
+rainbowBtn.onclick = () => setCurrentTool("rainbow");
+eraserBtn.onclick = () => setCurrentTool("eraser");
+clearBtn.onclick = () => clearGrid();
 densityBtn.addEventListener(`click`, changeGrid);
-
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
